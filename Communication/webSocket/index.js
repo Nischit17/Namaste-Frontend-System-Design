9a101dc0ef51483553c4bd4fedbc0d514011aaf5
Nischit17 <nischit819@gmail.com>
2025-05-09 20:45:32 +0530

    Learnt how webSockets works under the hood and built an server example

---
+++ Communication/webSocket/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const { createServer } = require("node:http");
+const { join } = require("node:path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+io.on("connection", (socket) => {
+  console.log("Connection Established");
+
+  socket.on("chat message", (msg) => {
+    console.log("received message", msg);
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on PORT: ${PORT}`);
+});
